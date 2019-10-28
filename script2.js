c1faffecd9146b7e2c7ec7081a5a22db326c6285
Zatelbeeit <breaul@gmail.com>
2019-10-28 16:57:48 +0100

    another commit before adding hobbies

---
+++ script2.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    var hobbies = ["Vegan", "Ecologie - Bio", "Fait maison - Fait main", "Acheter & vivre local", "Relax - Cool - Repos",
+        "Epicurien - Carpe Diem", "Ouvert d'esprit", "A l'improviste - Imprévu", "Luxe - Mode - Tendance", "Moto", "Collectionneur", "monomaniaque"]
+})
